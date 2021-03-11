aab564ea77fef882c2a3c09caa8716d8db871c71
g.cathy <cathy.gay@students.hevs.ch>
2021-03-11 16:28:12 +0100

    Add UI (navigation, first pages)

--- app/src/main/java/ch/dc/shipment_tracking_app/MainActivity.java
+++ app/src/main/java/ch/dc/shipment_tracking_app/MainActivity.java
@@ -1,14 +1,81 @@
 package ch.dc.shipment_tracking_app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Make our toolbar as the action bar.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Add the menu button on the app bar and moves it dynamically when the drawer opens or closes.
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    public void goClientSide (View view) {
+        redirectActivity(this, ClientMainActivity.class);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                redirectActivity(this, MainActivity.class);
+                break;
+            case R.id.nav_settings:
+                redirectActivity(this, SettingsActivity.class);
+                break;
+            case R.id.nav_about_us:
+                redirectActivity(this, AboutUsActivity.class);
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //When the drawer is open, pressing on the back button will close the drawer instead of going back.
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity, aClass);
+        activity.startActivity(intent);
     }
 }
